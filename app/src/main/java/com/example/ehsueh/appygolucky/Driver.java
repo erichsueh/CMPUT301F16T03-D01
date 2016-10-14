6073f5a33e5d3244948e0a2d75b4f67034092bad
Corey <hunt1@ualberta.ca>
2016-10-13 22:20:40 -0600

    added user, driver, and rider classes

---
+++ app/src/main/java/com/example/ehsueh/appygolucky/Driver.java
@@ -0,0 +1,8 @@
+package com.example.ehsueh.appygolucky;
+
+/**
+ * Created by Corey on 2016-10-13.
+ */
+
+public class Driver extends User {
+}
---
+++ app/src/main/java/com/example/ehsueh/appygolucky/User.java
@@ -0,0 +1,16 @@
+package com.example.ehsueh.appygolucky;
+
+/**
+ * Created by Corey on 2016-10-13.
+ */
+
+public class User {
+    private String username;
+    private String name;
+    private String email;
+    private String phone;
+
+    public User() {
+
+    }
+}
