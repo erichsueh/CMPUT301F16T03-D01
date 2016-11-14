d758b708cda5ee87f5ee162d8d467903cb9d03cb
erichsueh <erichsueh1337@hotmail.com>
2016-11-14 02:31:29 -0700

    Final edit for tonight, going to bed, added Edit Profile Stuff, will finish it tomorrow

--- app/src/main/java/com/example/ehsueh/appygolucky/EditProfile.java
+++ app/src/main/java/com/example/ehsueh/appygolucky/EditProfile.java
@@ -1,7 +1,9 @@
 package com.example.ehsueh.appygolucky;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditProfile extends ActionBarActivity {
 
@@ -9,5 +11,9 @@ public class EditProfile extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+    public void AddRequest(View view) {
+        finish();
     }
 }
