adc47978c89ffa80e25d46dc9c87f907cc825c1d
erichsueh <erichsueh1337@hotmail.com>
2016-11-21 13:19:17 -0700

    Colors work finally

--- app/src/main/java/com/example/ehsueh/appygolucky/RiderRequestListActivity.java
+++ app/src/main/java/com/example/ehsueh/appygolucky/RiderRequestListActivity.java
@@ -2,10 +2,12 @@ package com.example.ehsueh.appygolucky;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -33,7 +35,27 @@ public class RiderRequestListActivity extends ActionBarActivity {
 
         ArrayList<Ride> rides = uc.getCurrentUser().getAcceptedRides();
         final ArrayList<Ride> list = new ArrayList<Ride>(rides);
-        final ArrayAdapter rideAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        final ArrayAdapter rideAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position,convertView,parent);
+                Ride ride = list.get(position);
+                if(ride.getStatus()==2)
+                {
+                    view.setBackgroundColor(Color.parseColor("#00FF00"));
+                }
+                else if(ride.getStatus()==1)
+                {
+                    view.setBackgroundColor(Color.parseColor("#FFFF00"));
+                }
+                else
+                {
+                    view.setBackgroundColor(Color.parseColor("#000000"));
+                }
+                return view;
+            }
+        };
         listView.setAdapter(rideAdapter);
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -42,7 +64,7 @@ public class RiderRequestListActivity extends ActionBarActivity {
                 AlertDialog.Builder adb = new AlertDialog.Builder(RiderRequestListActivity.this);
                 final int finalPosition = position;
                 Ride ride = list.get(finalPosition);
-                if (ride.getStatus()==0) {
+                if (ride.getStatus()==1) {
                     adb.setMessage("What would you like to do?");
                     adb.setCancelable(true);
 
@@ -56,7 +78,7 @@ public class RiderRequestListActivity extends ActionBarActivity {
                     adb.setNeutralButton("See List", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent = new Intent(RiderRequestListActivity.this, DriverPendingActivity.class);
+                            Intent intent = new Intent(RiderRequestListActivity.this, ListDriversChooseActivity.class);
                             startActivity(intent);
                         }
                     });
@@ -65,7 +87,7 @@ public class RiderRequestListActivity extends ActionBarActivity {
                         public void onClick(DialogInterface dialogInterface, int i) {
                         }
                     });
-                } else if (ride.getStatus()==1) {
+                } else if (ride.getStatus()==0) {
                     adb.setMessage("Confirm Deletion?");
                     adb.setCancelable(true);
                     adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
