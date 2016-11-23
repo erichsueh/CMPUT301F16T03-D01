ba52dcf76c3b9012071ea3785e3d976cb4e4ba44
md6113 <jmdougla@ualberta.ca>
2016-11-23 16:33:43 -0700

    The beginnings of the online/offline behaviour are beginning to take form in this branch. NOTE: The app should not compile at this point.

--- app/src/main/java/com/example/ehsueh/appygolucky/MainActivity.java
+++ app/src/main/java/com/example/ehsueh/appygolucky/MainActivity.java
@@ -20,13 +20,16 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package com.example.ehsueh.appygolucky;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.List;
 
 /**
@@ -36,6 +39,24 @@ import java.util.List;
  */
 public class MainActivity extends ActionBarActivity {
     UserController uc;
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.example.android.datasync.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "example.com";
+    // The account name
+    public static final String ACCOUNT = "default_account";
+    // Sync interval constants
+    public static final long SECONDS_PER_MINUTE = 60L;
+    public static final long SYNC_INTERVAL_IN_MINUTES = 60L;
+    public static final long SYNC_INTERVAL =
+            SYNC_INTERVAL_IN_MINUTES *
+                    SECONDS_PER_MINUTE;
+    // Global variables
+    // A content resolver for accessing the provider
+    ContentResolver mResolver;
+    // Instance fields
+    Account mAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +70,18 @@ public class MainActivity extends ActionBarActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create the dummy account
+        mAccount = CreateSyncAccount(this);
+        // Get the content resolver for your app
+        mResolver = getContentResolver();
+        /*
+         * Turn on periodic syncing
+         */
+        ContentResolver.addPeriodicSync(
+                mAccount,
+                AUTHORITY,
+                Bundle.EMPTY,
+                SYNC_INTERVAL);
     }
 
     public void LoginClick(View view) {
@@ -119,6 +152,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
 
+    }
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return null;
     }
 
 //    I don't think we need the three dot on main screen
---
+++ app/src/main/java/com/example/ehsueh/appygolucky/OfflineOnline/NetworkErrorException.java
@@ -0,0 +1,7 @@
+package com.example.ehsueh.appygolucky.OfflineOnline;
+
+/**
+ * Created by Maxwell on 2016-11-22.
+ */
+public class NetworkErrorException extends Exception {
+}
