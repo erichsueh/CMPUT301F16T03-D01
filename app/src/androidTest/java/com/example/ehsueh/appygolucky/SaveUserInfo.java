b01e8d31fde7d7fe363031cb1e08e6fe0bcf5be1
Corey <hunt1@ualberta.ca>
2016-11-25 22:36:47 -0700

    tidied up SaveUserInfo a little (the test)

--- app/src/androidTest/java/com/example/ehsueh/appygolucky/SaveUserInfo.java
+++ app/src/androidTest/java/com/example/ehsueh/appygolucky/SaveUserInfo.java
@@ -50,14 +50,11 @@ public class SaveUserInfo extends ActivityInstrumentationTestCase2 {
 
         Ride rideA = new Ride(startLocationA, endLocationA, fareA, rideDescriptionA, riderA);
         Ride rideB = new Ride(startLocationB, endLocationB, fareB, rideDescriptionB, riderB);
-        rideB.addDriverUsername(riderA.getUsername());
         Ride rideC = new Ride(startLocationC, endLocationC, fareC, rideDescriptionC, riderC);
-        rideC.addDriverUsername(riderB.getUsername());
 
         uc.addRideRequest(rideA);
         uc.addRideRequest(rideB);
         uc.addRideRequest(rideC);
-        uc.addAcceptedRequest(rideA);
         uc.addAcceptedRequest(rideB);
         uc.addAcceptedRequest(rideC);
 
