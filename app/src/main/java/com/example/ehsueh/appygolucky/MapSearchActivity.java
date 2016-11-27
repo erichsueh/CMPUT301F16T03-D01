31f749d1ff61bf5b13ff4d37e5484b3904d41ffe
erichsueh <erichsueh1337@hotmail.com>
2016-11-26 23:30:17 -0700

    Searchpage updated

---
+++ app/src/main/java/com/example/ehsueh/appygolucky/MapSearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.ehsueh.appygolucky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_search);
+    }
+}
--- app/src/main/java/com/example/ehsueh/appygolucky/SearchPageActivity.java
+++ app/src/main/java/com/example/ehsueh/appygolucky/SearchPageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -17,22 +18,35 @@ import com.google.android.gms.maps.model.LatLng;
  */
 
 public class SearchPageActivity extends ActionBarActivity {
-
+    private EditText searchText;
+    //private UserController uc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+        searchText = (EditText) findViewById(R.id.editText);
+        //uc = new UserController(getApplicationContext());
     }
 
+    public void searchWithLocation(View view){
+        String searchLocation = searchText.getText().toString();
+        //ElasticSearchRideController.GetRidesByLocationTask(searchLocation);
+
+    }
+
+    /**
+     * renaim this to "back"
+     * @param view
+     */
     public void AcceptSearch (View view) {
         finish();}
 
     public void ShowMap(View view){
-        Intent intent = new Intent(SearchPageActivity.this, MapsActivity.class);
-        LatLng start_location= new LatLng(53.5444, -113.4909);
-        LatLng end_location= new LatLng(53.525288, -113.525454);
-        intent.putExtra("start",start_location);
-        intent.putExtra("end",end_location);
+        Intent intent = new Intent(SearchPageActivity.this, MapSearchActivity.class);
+        //LatLng start_location= new LatLng(53.5444, -113.4909);
+        //LatLng end_location= new LatLng(53.525288, -113.525454);
+        //intent.putExtra("start",start_location);
+        //intent.putExtra("end",end_location);
         startActivity(intent);
     }
 }
